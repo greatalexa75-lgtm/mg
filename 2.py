d4776cee096d092855d6eb4d1b97c1b676969990
greatalexa75-lgtm <greatalexa75@gmail.com>
2026-02-10 12:53:24 +0530

    Update database connection parameters in 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,8 @@
 import mysqldb as sql
 host="localhost"
-user="appu.s"
+user="mg"
 password="root"
 db='python_db'
 conn=sql.connect(host=hosst,user=user,password=password,db=db)
+
 cur=conn.cursor()
